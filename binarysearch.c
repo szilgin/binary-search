013e2631db2feeb8b6234d71a35cd6a5941b58c9
szilgin <szilgin@gmail.com>
2016-08-12 10:56:44 +0300

    Create binarysearch.c

---
+++ binarysearch.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int compare(const void *key , const void *val_at_index)
+{
+    return *(size_t*)key -  *(size_t*)val_at_index; 
+}
+
+
+                                                 // length ,   
+ int bsearch(const void *key, const void *base, size_t nmemb, int (*compar)(const void *, const void *))
+{
+  size_t index_1;
+  size_t index 2;
+  
+  while(index_1 < index_2)
+  {
+  
+    if ( compare(const void *key , const void *val_at_index)
+    {
+          return index
+    }
+
+
+
+
+}
