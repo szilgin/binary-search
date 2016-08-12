1b229e15de9e966ceb7c7350bbbefd377dd9b1d4
szilgin <szilgin@gmail.com>
2016-08-12 11:51:12 +0300

    Update binarysearch.c

--- binarysearch.c
+++ binarysearch.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
-#include <stdio.h>
- #include <stdlib.h>
+#include <stdio.h
+#include <stdlib.h>
 int Compare(const void *key , const void *val_at_index)
 {
     return *(size_t*)key -  *(size_t*)val_at_index; 
